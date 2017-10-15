89c156ebb7d81e58c5afebca1e5e51b6200e56f5
xsigmar <vojta.marek@gmail.com>
2017-10-15 23:09:02 +0200

    knows if number or word

--- main.c
+++ main.c
@@ -1,11 +1,69 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <limits.h>
 
-int a;
+/*
+int delkaRetezce(char *data)
+{
+    int delka = 0;
+
+    while (data[delka] != '\0')
+    {
+        delka++;
+    }
+
+    return delka;
+}
+ú
+*/
+
+int jeCislo(char *data)
+{
+  int c;
+  int cislo = 0;
+  int delka = strlen(data);
+
+  for (c = 0; c < delka; c++) //iteruje do konce delky retezce
+  {
+      if(isdigit(data[c]) == 0) // pokud pomoci funkce isdigit narazi na neciselny znak breakne a vrati hodnotu 0 -> nejedna se o cislo
+      {
+          cislo = 0;
+          break;
+      }
+      else
+      {
+          cislo = 1; //pokud doiteruje do konce retezce bez nalezeni necisleneho znaku vraci 1 -> jedna se o cislo
+      }
+
+  }
+  return cislo;
+}
+
+int vstup;
+char data[101];
 /*int argc, char const *argv[]*/
-int main(void) {
+int main(void)
+{
 
-  printf ("This is a stdin test:\n");
-  a = stdin();
-  printf("%d\n", a);
+  printf ("Zadej data:\n");
+  vstup = scanf("%s", data); //uloží vstup z klávesnice do pole data
+
+  while (vstup != EOF)
+  {
+    if (jeCislo(data) == 1)
+    {
+      int cisloVstup = atoi(data);
+      printf("Číslo, které jste zadali je: %d\n", cisloVstup);
+    }
+
+    else if (jeCislo(data) == 0)
+    {
+      printf("Slovo, které jste zadali je: %s\n", data);
+    }
+
+    vstup = scanf("%s", data); //ceka stale na dalsi vstup
+  }
   return 0;
 }
