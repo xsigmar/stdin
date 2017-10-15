a1cdb09921fb53f8da7539b59cdd7aa8c2e9b886
xsigmar <vojta.marek@gmail.com>
2017-10-15 22:29:47 +0200

    1st

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-
+int a;
 /*int argc, char const *argv[]*/
 int main(void) {
 
-  printf ("This is a stdin test\n");
-
+  printf ("This is a stdin test:\n");
+  a = stdin();
+  printf("%d\n", a);
   return 0;
 }
